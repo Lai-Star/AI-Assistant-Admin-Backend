e1e211818b3bc8b2c34e6014104723f1b94f9ea7
Sayaka <ishibashi771118@gmail.com>
2025-04-18 02:31:59 -0700

    Set up CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ai-assistant-admin-frontend.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
